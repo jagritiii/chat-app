5098b4a47a84f749585c9f8fc6f72104d93efbe0
diwanatharv <atharvdiwan26@gmail.com>
2024-07-24 22:01:04 +0530

    india

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	panic("idnia")
+	fmt.Println("after panic")
+    // unreachable code
+    fmt.Println("this line will not be executed")
 }
+
